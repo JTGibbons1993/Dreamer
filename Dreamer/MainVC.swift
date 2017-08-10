a7527d09cb63f01ff9c00c43da00b9b456bdcef1
JTGibbons1993 <jtgibbons1993@gmail.com>
2017-08-10 12:14:18 -0400

    Setup Segmented Control Action to sort Data

--- Dreamer/MainVC.swift
+++ Dreamer/MainVC.swift
@@ -75,7 +75,16 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         
         let fetchRequest: NSFetchRequest<Item> = Item.fetchRequest() //as! NSFetchRequest<Item>
         let dateSort = NSSortDescriptor(key: "created", ascending: false)
-        fetchRequest.sortDescriptors = [dateSort]
+        let priceSort = NSSortDescriptor(key: "price", ascending: true)
+        let titleSort = NSSortDescriptor(key: "title", ascending: true)
+        
+        if segment.selectedSegmentIndex == 0 {
+            fetchRequest.sortDescriptors = [dateSort]
+        } else if segment.selectedSegmentIndex == 1 {
+            fetchRequest.sortDescriptors = [priceSort]
+        } else {
+            fetchRequest.sortDescriptors = [titleSort]
+        }
         
         
         let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
@@ -89,6 +98,12 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
             print(error)
         }
     }
+    
+    @IBAction func sortingSegmentSelected(_ sender: Any) {
+        attemptFetch()
+        tableView.reloadData()
+    }
+    
     
     func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
         tableView.beginUpdates()
